a389f05b4d1f51b7c207c976a6ab86f7af597ba2
xushnudbek-prog <xushnudbek756@gmail.com>
2026-01-07 10:45:55 +0500

    feat: Implement initial Spring Boot application with a basic controller, Dockerfile, CI workflow, and Actuator/Prometheus configuration.

---
+++ src/main/java/app/platform/agriculture_app/controller/HelloController.java
@@ -0,0 +1,14 @@
+package app.platform.agriculture_app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/home")
+    public String home() {
+        return "Agri DevOps Demo: running";
+    }
+
+}
